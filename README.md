28aa7bce60497aa015e634c9b6ac03632d8ec6c3
NickMasters <master-7@inbox.ru>
2018-02-18 18:18:37 +0300

    Timeout tasks ready

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ yarn && yarn start
 If you use the Postman just import collection ``postman_collection.json`` and run ``EchoAtTime``
 
 You can change query params in ``body``
+
+```
+{
+   	"time": "18:13",
+   	"message": "Some custom message"
+}
+```
+
+``time`` the time when you'll see msg on console
+
+``message`` a message to be printed
--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const Koa = require('koa');
 const KoaRouter = require('koa-router');
 const bodyParser = require('koa-bodyparser');
-const redis = require("redis");
+const moment = require('moment');
+const thenRedisClient = require('then-redis');
 
 const app = new Koa();
 const router = new KoaRouter();
-const RedisClient = redis.createClient();
+const db = thenRedisClient.createClient();
 
 router.get('/', async ctx => {
     ctx.body = 'Hello World!';
 });
 
 router.post('/echoAtTime', async ctx => {
-    ctx.body = 'Hello World!';
+    const {time, message} = ctx.request.body;
+    db.set(time, message);
+    ctx.body = 'Task created!';
 });
+
+setInterval(async () => {
+    const time = moment().format('HH:mm');
+    const result = await db.get(time);
+    if (result) {
+        console.log(result);
+    }
+    db.del(time);
+}, 5000);
 
 app
     .use(bodyParser())
